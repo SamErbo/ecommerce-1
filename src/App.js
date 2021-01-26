6728cacd3d9b6cf2f81e7fc50c3d9020179e4d4d
SamErbo <samuel.ermias@gmail.com>
2021-01-26 18:32:52 +0300

    created project with react-app and added file to the home page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Homepage from './homepage.component'
+
+function App() {
+  return (
+    <div>
+     <Homepage />
+    </div>
+  );
+}
+
+export default App;
